7f5112bd0d23a0fbfbf86cb7f8c7dc0201880514
Adriano Vieira <tsiadriano@gmail.com>
2020-01-12 16:43:24 -0300

    Correção banco de dados

--- sql_db_crud.sql
+++ sql_db_crud.sql
@@ -13,58 +13,16 @@ CREATE SCHEMA IF NOT EXISTS `db_crud` DEFAULT CHARACTER SET utf8 ;
 USE `db_crud` ;
 
 -- -----------------------------------------------------
--- Table `db_crud`.`telefone`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_crud`.`telefone` (
-  `id_telefone` INT NOT NULL,
-  `nome` VARCHAR(25) NOT NULL,
-  `numero` VARCHAR(25) NOT NULL,
-  PRIMARY KEY (`id_telefone`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `db_crud`.`endereco`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `db_crud`.`endereco` (
-  `id_endereco` INT NOT NULL,
-  `nome` VARCHAR(25) NOT NULL,
-  `cep` VARCHAR(9) NOT NULL,
-  `logradouro` VARCHAR(100) NOT NULL,
-  `bairro` VARCHAR(100) NULL,
-  `cidade` VARCHAR(100) NOT NULL,
-  `estado` VARCHAR(100) NOT NULL,
-  `numero` VARCHAR(10) NULL,
-  `ref` VARCHAR(60) NULL,
-  PRIMARY KEY (`id_endereco`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `db_crud`.`p_fisica`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_crud`.`p_fisica` (
-  `id_fisica` INT NOT NULL,
+  `id_fisica` INT NOT NULL AUTO_INCREMENT,
   `cpf` VARCHAR(14) NOT NULL,
   `nome` VARCHAR(100) NOT NULL,
   `rg` VARCHAR(14) NOT NULL,
   `sexo` VARCHAR(25) NOT NULL,
   `dt_nascimento` DATE NOT NULL,
-  `id_telefone` INT NOT NULL,
-  `id_endereco` INT NOT NULL,
-  PRIMARY KEY (`id_fisica`),
-  INDEX `fk_p_fisica_telefone_idx` (`id_telefone` ASC),
-  INDEX `fk_p_fisica_endereco1_idx` (`id_endereco` ASC),
-  CONSTRAINT `fk_p_fisica_telefone`
-    FOREIGN KEY (`id_telefone`)
-    REFERENCES `db_crud`.`telefone` (`id_telefone`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_p_fisica_endereco1`
-    FOREIGN KEY (`id_endereco`)
-    REFERENCES `db_crud`.`endereco` (`id_endereco`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  PRIMARY KEY (`id_fisica`))
 ENGINE = InnoDB;
 
 
@@ -72,25 +30,67 @@ ENGINE = InnoDB;
 -- Table `db_crud`.`p_juridica`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `db_crud`.`p_juridica` (
-  `id_juridico` INT NOT NULL,
+  `id_juridico` INT NOT NULL AUTO_INCREMENT,
   `cnpj` VARCHAR(18) NOT NULL,
   `razao_social` VARCHAR(100) NOT NULL,
   `nome_fantasia` VARCHAR(100) NOT NULL,
   `inscricao_estadual` VARCHAR(11) NOT NULL,
   `dt_fundacao` DATE NOT NULL,
-  `id_telefone` INT NOT NULL,
-  `id_endereco` INT NOT NULL,
-  PRIMARY KEY (`id_juridico`),
-  INDEX `fk_p_juridica_telefone1_idx` (`id_telefone` ASC),
-  INDEX `fk_p_juridica_endereco1_idx` (`id_endereco` ASC),
-  CONSTRAINT `fk_p_juridica_telefone1`
-    FOREIGN KEY (`id_telefone`)
-    REFERENCES `db_crud`.`telefone` (`id_telefone`)
+  PRIMARY KEY (`id_juridico`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_crud`.`telefone`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_crud`.`telefone` (
+  `id_telefone` INT NOT NULL AUTO_INCREMENT,
+  `tipoT` VARCHAR(25) NOT NULL,
+  `numero` VARCHAR(25) NOT NULL,
+  `id_fisica` INT NULL,
+  `id_juridico` INT NULL,
+  PRIMARY KEY (`id_telefone`),
+  INDEX `fk_telefone_p_fisica_idx` (`id_fisica` ASC) ,
+  INDEX `fk_telefone_p_juridica1_idx` (`id_juridico` ASC),
+  CONSTRAINT `fk_telefone_p_fisica`
+    FOREIGN KEY (`id_fisica`)
+    REFERENCES `db_crud`.`p_fisica` (`id_fisica`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_p_juridica_endereco1`
-    FOREIGN KEY (`id_endereco`)
-    REFERENCES `db_crud`.`endereco` (`id_endereco`)
+  CONSTRAINT `fk_telefone_p_juridica1`
+    FOREIGN KEY (`id_juridico`)
+    REFERENCES `db_crud`.`p_juridica` (`id_juridico`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `db_crud`.`endereco`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `db_crud`.`endereco` (
+  `id_endereco` INT NOT NULL AUTO_INCREMENT,
+  `tipoE` VARCHAR(25) NOT NULL,
+  `cep` VARCHAR(9) NOT NULL,
+  `logradouro` VARCHAR(100) NOT NULL,
+  `bairro` VARCHAR(100) NULL,
+  `cidade` VARCHAR(100) NOT NULL,
+  `estado` VARCHAR(100) NOT NULL,
+  `numero` VARCHAR(10) NULL,
+  `ref` VARCHAR(60) NULL,
+  `id_fisica` INT NULL,
+  `id_juridico` INT NULL,
+  PRIMARY KEY (`id_endereco`),
+  INDEX `fk_endereco_p_fisica1_idx` (`id_fisica` ASC),
+  INDEX `fk_endereco_p_juridica1_idx` (`id_juridico` ASC),
+  CONSTRAINT `fk_endereco_p_fisica1`
+    FOREIGN KEY (`id_fisica`)
+    REFERENCES `db_crud`.`p_fisica` (`id_fisica`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_endereco_p_juridica1`
+    FOREIGN KEY (`id_juridico`)
+    REFERENCES `db_crud`.`p_juridica` (`id_juridico`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
